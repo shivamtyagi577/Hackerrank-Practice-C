af6b2cac7341bc921d37a1123cbc5fa0cb2a26ca
shivamtyagi577 <39437296+shivamtyagi577@users.noreply.github.com>
2018-05-22 22:11:28 +0200

    Update staircase.c

--- staircase.c
+++ staircase.c
@@ -33,3 +33,39 @@ int main()
     printf("\n");
  }
 }
+
+//----------------------------------------Another solution------------------------
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int m,i,j;
+  m=n;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(j>=m-1)
+      {
+        printf("#");
+      }
+      else
+      {
+      printf(" ");  
+      }
+    }
+    printf("\n");
+    m=m-1;
+  }
+}
