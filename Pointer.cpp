23de56ef63e744b44df3c8fc8e2fa33a0d85642e
shivamtyagi577 <39437296+shivamtyagi577@users.noreply.github.com>
2019-04-27 20:35:33 +0530

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int c=*a;  
+    *a=*a + *b;
+    *b=c - *b;
+    if(*b<1)
+    {
+        *b=(*b)*(-1);
+    }   
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
